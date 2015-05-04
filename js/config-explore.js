c47fad701efc5c8ec7efe7527458779a1180d3f1
shoeswat <ymondal@berkeley.edu>
2015-05-04 12:54:59 -0700

    fixes issue #101

--- js/config-explore.js
+++ js/config-explore.js
@@ -7,8 +7,8 @@ ECO.endpoints = {
   sensors: 'https://ecoengine.berkeley.edu/api/sensors/?page_size=5000&format=geojson',
   layers: 'https://ecoengine.berkeley.edu/api/layers/',
   rasters: STMN ? STMN.rstorePath : "",
-  reserves: 'https://ecoengine.berkeley.edu/api/layers/reserves/features/?page_size=500&order_by=name',
-  jepson: 'https://ecoengine.berkeley.edu/api/layers/jepson-regions/features/?page_size=500&order_by=name'
+  reserves: 'https://ecoengine.berkeley.edu/api/layers/reserves/features/?ordering=name',
+  jepson: 'https://ecoengine.berkeley.edu/api/layers/jepson-regions/features/?ordering=name'
 };
 
 ECO.basemaps = {
